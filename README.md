3744adfc02505031d77d5c2b9b80090d33484775
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2023-05-06 10:15:45 +0700

    Update readme

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Nostr-ChatBot
 
 Make Nostr ChatBot from python
+
+## Install
+
+> pip install nostr-chatbot
+
+## Example
+
+See [https://github.com/wannaphong/nostr-chatbot/tree/main/example](https://github.com/wannaphong/nostr-chatbot/tree/main/example)
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt","r",encoding="utf-8-sig") as f:
 
 setup(
     name="nostr-chatbot",
-    version="0.1.0",
+    version="0.1.0dev1",
     description="Nostr chatbotlibrary",
     long_description=readme,
     long_description_content_type="text/markdown",
